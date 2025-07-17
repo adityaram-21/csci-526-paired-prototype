2472786532480a07e318833ba00e8491c84f7fe2
Aditya Ramachandran <adityar2@usc.edu>
2025-07-17 01:21:50 -0700

    added pause/play functionality

--- Assets/Scripts/GameOverManager.cs
+++ Assets/Scripts/GameOverManager.cs
@@ -1,9 +1,41 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverManager : MonoBehaviour
 {
     public GameObject gameOverUI; // Assign the Game Over UI in the inspector
+    public TMP_Text titleText;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                ResumeGame();
+            else
+                PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        isPaused = true;
+        gameOverUI.SetActive(true);
+        Time.timeScale = 0f;
+        titleText.text = "Game Paused";
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        gameOverUI.SetActive(false);
+        Time.timeScale = 1f;
+        titleText.text = "Game Over";
+    }
+
     public void TriggerGameOver()
     {
         gameOverUI.SetActive(true);
